dc23fecd877b87381f02762fe1bbf5cc3d136dd8
batyrkhana <batyrkhansa@gmail.com>
2023-10-13 09:18:52 -0400

    d1

---
+++ dom.js
@@ -0,0 +1,54 @@
+const DOMSelectors = {
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Directions for the project
+
+Using JavaScript you will allow users to input information into a form. The
+form will then push data from a JavaScript object into the HTML through
+DOM manipulation. The project must contain the following
+-Create an object called “DOMSelectors” to hold your DOM Selectors
+-Create a function that creates an object and calls the following functions
+-Create a function that injects the newly created object into the DOM
+-Create a function that clears the input fields after injecting the object
+-Create a function to remove an object after they have been created 
+*/
+
+/*
+CLASS DEMO
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+    h2s: document.querySelectorAll("h2"),
+};
+console.log(DOMSelectors.firstName),
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach((el)=>el.textContent = DOMSelectors.firstName.value
+    );
+});
+*/
